8cd0a46e44df2e78e1a1e097ed8ce454e55a26e1
Ciro Santilli <ciro.santilli@gmail.com>
2014-05-26 23:29:44 +0200

    Js prototype info.

---
+++ phantomjs/main.js
@@ -0,0 +1,32 @@
+//#console
+
+  console.log('stdout')
+
+//#webpage
+
+  var webpage = require('webpage')
+
+  var page = webpage.create()
+  page.content = '<!DOCTYPE html><title></title>'
+    + '<body style="background-color:white;">'
+    + '  <h1>Rendered by PhantomJS</h1>'
+    + '  <p>Paragraph.</p>'
+    + '  <p style="color:red;">Red paragraph.</p>'
+    + '</body>'
+  page.render('render.png')
+
+  // Download webpage from URL.
+  var page = webpage.create()
+  page.open('http://www.phantomjs.org/', function (status) {
+    if (status !== 'success') {
+      console.log('failed to load page')
+    } else {
+      console.log(page.content)
+    }
+  })
+
+//#exit
+
+  // Mandatory, or else program will not exit.
+
+  phantom.exit();
