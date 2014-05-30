8d43f1fd19af6ec1f00f45012d442bb96049645c
Ciro Santilli <ciro.santilli@gmail.com>
2014-05-31 00:25:03 +0200

    Better assertion, DOMContentLoaded, IIFE syntax details.

--- assert.js
+++ assert.js
@@ -1,41 +1,51 @@
-var all_asserts_passed = true
+// Make assertions and show useful fail information on top of the web page.
+
+var error_message = ''
+
+function report_error(extra) {
+  var err = new Error('AssertFailed')
+  error_message += '<h1 style="background-color:red;">ASSERT FAILED</h1><pre>'
+    + err.stack + '</pre>' + String(extra)
+}
+
+function report_error_eq(actual, expect) {
+  report_error("<pre>actual = " + actual + "\nexpect = " + expect + "</pre>")
+}
 
 function assert(tf) {
   if (!tf) {
-    all_asserts_passed = false
-    document.write("<h1>ASSERT FAILED</h1>")
-    var err = new Error('AssertFailed')
-    document.write("<pre>" + err.stack + "</pre>")
-    throw err
+    report_error()
   }
 }
 
 function assert_eq(actual, expect) {
-  if (! (actual === expect)) {
-    all_asserts_passed = false
-    document.write("<h1>ASSERT FAILED</h1>")
-    var err = new Error('AssertFailed')
-    document.write("<pre>" + err.stack + "</pre>")
-    document.write("<pre>actual = " + actual + "\nexpect = " + expect + "</pre>")
-    throw err
+  if (actual !== expect) {
+    report_error_eq(actual, expect)
   }
 }
 
 function assert_neq(actual, expect) {
   if (actual === expect) {
-    all_asserts_passed = false
-    document.write("<h1>ASSERT FAILED</h1>")
-    var err = new Error('AssertFailed')
-    document.write("<pre>" + err.stack + "</pre>")
-    document.write("<pre>actual = " + actual + "\nexpect = " + expect + "</pre>")
-    throw err
+    report_error_eq(actual, expect)
   }
 }
 
 // Test our asserts.
-
 assert(true)
 //assert(false)
 assert_eq(0, 0)
 //assert_eq(0, 1)
 assert_neq(0, 1)
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function() {
+    var body = document.getElementsByTagName('body')[0]
+    if (error_message) {
+      body.innerHTML = error_message + body.innerHTML
+    } else {
+      body.innerHTML = '<h1 style="background-color:green;">ALL ASSERTS PASSED</h1>' + body.innerHTML
+    }
+  },
+  true
+)
