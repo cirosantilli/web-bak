1c4d993c6d8191938ae0db1e5725217319767ba8
Ciro Santilli <ciro.santilli@gmail.com>
2014-06-13 00:23:20 +0200

    new Image vs document.createElement
    Simplify assert.js: immediately show error message on fail.

--- assert.js
+++ assert.js
@@ -1,15 +1,14 @@
 // Make assertions and show useful fail information on top of the web page.
 
-var error_message = ''
-
 function report_error(extra) {
   var err = new Error('AssertFailed')
-  error_message += '<div style="background-color:red; border: 1px solid black;'
+  var error_message = '<div style="background-color:red; border: 1px solid black;'
     + 'font-size:36px; font-weight:bold; padding:3px;">ASSERT FAILED</div><pre>'
     + err.stack + '</pre>'
   if (extra) {
     error_message += '<pre>' + String(extra) + '</pre>'
   }
+  document.body.insertAdjacentHTML('afterBegin', error_message)
 }
 
 function report_error_eq(actual, expect) {
@@ -63,14 +62,3 @@ assert(true)
 assert_eq(0, 0)
 //assert_eq(0, 1)
 assert_neq(0, 1)
-
-document.addEventListener(
-  'DOMContentLoaded',
-  function() {
-    var body = document.body
-    if (error_message) {
-      document.body.insertAdjacentHTML('afterBegin',  error_message)
-    }
-  },
-  true
-)
